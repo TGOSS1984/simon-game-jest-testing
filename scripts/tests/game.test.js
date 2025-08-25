742ac435f87e59b08d3cdb0f120f99b5a1b3a0eb
TGOSS1984 <tomgoss84@gmail.com>
2025-08-25 16:47:50 +0100

    add code for turn in progress so cannot click during computer turn and test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -35,6 +35,15 @@ describe("game object contains correct keys", () => {
   test("turnNumber key exists", () => {
     expect("turnNumber" in game).toBe(true);
   });
+  test("lastButton key exists", () => {
+    expect("lastButton" in game).toBe(true);
+  });
+  test("turnInProgress key exists", () => {
+    expect("turnInProgress" in game).toBe(true);
+  });
+  test("turnInProgress key value is false", () => {
+    expect("turnInProgress" in game).toBe(true);
+  });
 });
 
 describe("newGame works correctly", () => {
@@ -101,5 +110,15 @@ describe("gameplay works correctly", () => {
         game.playerMoves.push("wrong");
         playerTurn();
         expect(window.alert).toHaveBeenCalledWith("Wrong move!");
+    });
+    test("should toggle turnInProgress to true",() => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during the computer sequence should fail",() => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
     });
 });
